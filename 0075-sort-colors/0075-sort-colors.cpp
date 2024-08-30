7ec992ae38dc1bf70efc28ff0f225bea94de1bbf
Vidushi Agarwal <134790933+vidushi-vi@users.noreply.github.com>
2024-08-30 12:42:39 +0530

    Time: 2 ms (43.64%), Space: 10.3 MB (13.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=0;j<nums.size()-1-i;j++)
+            {
+                if(nums[j]>nums[j+1])
+                    swap(nums[j+1],nums[j]);
+            }
+        }
+        
+    }
+};
