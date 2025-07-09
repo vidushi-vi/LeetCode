8f51637267f632e6f975c89ae47c5acbb8038f07
Vidushi Agarwal <134790933+vidushi-vi@users.noreply.github.com>
2025-07-09 18:26:56 +0530

    Time: 0 ms (100%), Space: 18.1 MB (15.24%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        ds=[]
+        def findSubsets(ind: int):
+            ans.append(ds[:])
+            for i in range(ind,len(nums)):
+                if i!=ind and nums[i]==nums[i-1]:
+                    continue
+                ds.append(nums[i])
+                findSubsets(i+1)
+                ds.pop()
+        nums.sort() 
+        findSubsets(0)
+        return ans
+
+        
