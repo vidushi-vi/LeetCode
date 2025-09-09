27c34ecd744c94c470d3b6c6a6e31d64f33c03b1
Vidushi Agarwal <134790933+vidushi-vi@users.noreply.github.com>
2025-09-09 20:44:42 +0530

    Time: 46 ms (39.64%), Space: 17.6 MB (89.42%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,18 +1,30 @@
 class Solution:
     def fib(self, n: int) -> int:
-        ''' brute force
+        ''' optimal'''
         
         if n<=1:
             return n
         a,b=0,1
         for i in range(2,n+1):
-            a,b=b,a+b
+            ans=a+b
+            a,b=b,ans
 
-        return b '''
+        return b 
         
-        # recursive code
+        '''# recursive code
         if n<=1:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        return self.fib(n-1)+self.fib(n-2) 
+
+        # DP Tabulation approach
+
+        if n<=1:
+            return n
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]'''
 
         
